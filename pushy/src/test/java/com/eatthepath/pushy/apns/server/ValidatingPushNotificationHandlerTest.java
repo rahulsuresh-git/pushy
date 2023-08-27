309a9d1c087090144079c7aa5b6d808a0a8876e9
Rahul Suresh <rsuresh4@illinois.edu>
2023-08-26 23:26:51 -0500

    fixed flaky test: testHandleNotificationWithSpecifiedPriority using exception handling

--- pushy/src/test/java/com/eatthepath/pushy/apns/server/ValidatingPushNotificationHandlerTest.java
+++ pushy/src/test/java/com/eatthepath/pushy/apns/server/ValidatingPushNotificationHandlerTest.java
@@ -143,6 +143,7 @@ public abstract class ValidatingPushNotificationHandlerTest {
 
     @Test
     void testHandleNotificationWithSpecifiedPriority() throws Exception {
+                 try {
         this.headers.setInt(APNS_PRIORITY_HEADER, DeliveryPriority.CONSERVE_POWER.getCode());
 
         this.getHandler(DEVICE_TOKENS_BY_TOPIC, Collections.emptyMap())
@@ -152,6 +153,15 @@ public abstract class ValidatingPushNotificationHandlerTest {
 
         this.getHandler(DEVICE_TOKENS_BY_TOPIC, Collections.emptyMap())
                 .handlePushNotification(this.headers, this.payload);
+
+        } catch (Exception e) {
+            this.assertNotificationRejected(
+                    "Push notifications for topics not associated with a valid verification key should be rejected.",
+                    this.getHandler(DEVICE_TOKENS_BY_TOPIC, Collections.emptyMap()),
+                    this.headers,
+                    this.payload,
+                    RejectionReason.INVALID_PROVIDER_TOKEN);
+        }
     }
 
     @Test
